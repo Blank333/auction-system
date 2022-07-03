c826470353696f8a0e4a585a9132387a63acc7c2
blank <60171262+Blank333@users.noreply.github.com>
2022-07-03 10:13:45 +0530

    Changed border size

--- components/varieties.js
+++ components/varieties.js
@@ -8,27 +8,27 @@ export default function Variety() {
                     <p className="text-center border-b-4 border-gray-500 bg-gray-300 p-1 rounded shadow-xl">Varieties</p>
                     <section className="flex flex-col text-center px-2 py-1">
                         
-                            <a className="mt-1 py-1 border border-gray-200 bg-gray-100 hover:border-4 hover:border-blue-100 hover:bg-gray-300 rounded shadow-lg">
+                            <a className="mt-1 py-1 border border-gray-200 bg-gray-100 hover:border-2 hover:border-blue-100 hover:bg-gray-300 rounded shadow-lg">
                                 Royal
                             </a>
                         
                         
-                            <a className="mt-1 py-1 border border-gray-200 bg-gray-100 hover:border-4 hover:border-blue-100 hover:bg-gray-300 rounded shadow-lg">
+                            <a className="mt-1 py-1 border border-gray-200 bg-gray-100 hover:border-2 hover:border-blue-100 hover:bg-gray-300 rounded shadow-lg">
                                 Golden
                             </a>
                         
                         
-                            <a className="mt-1 py-1 border border-gray-200 bg-gray-100 hover:border-4 hover:border-blue-100 hover:bg-gray-300 rounded shadow-lg">
+                            <a className="mt-1 py-1 border border-gray-200 bg-gray-100 hover:border-2 hover:border-blue-100 hover:bg-gray-300 rounded shadow-lg">
                                 Red Delicious
                             </a>
                         
                         
-                            <a className="mt-1 py-1 border border-gray-200 bg-gray-100 hover:border-4 hover:border-blue-100 hover:bg-gray-300 rounded shadow-lg">
+                            <a className="mt-1 py-1 border border-gray-200 bg-gray-100 hover:border-2 hover:border-blue-100 hover:bg-gray-300 rounded shadow-lg">
                                 Gale
                             </a>
                         
                         
-                            <a className="mt-1 py-1 border border-gray-200 bg-gray-100 hover:border-4 hover:border-blue-100 hover:bg-gray-300 rounded shadow-lg">
+                            <a className="mt-1 py-1 border border-gray-200 bg-gray-100 hover:border-2 hover:border-blue-100 hover:bg-gray-300 rounded shadow-lg">
                                 Granny Smith
                             </a>
                         
